bb2a9b7ca98b77c1ff30ec5cf495c238a619d230
manci <gmancillapaz@gmail.com>
2021-02-23 16:40:09 -0500

    commit inicial

---
+++ App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+     <View style={styles.contenedor}>
+      <View style={styles.caja1}></View>
+      <View style={styles.caja2}></View>
+      <View style={styles.caja3}></View>
+      <View style={styles.caja4}></View>
+     </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: 'cornflowerblue',
+    flex: 1,
+    flexDirection: 'row'
+  },
+  caja1: {
+    padding: 20,
+    backgroundColor: 'navy',
+  },
+  caja2: {
+    padding: 20,
+    backgroundColor: 'yellow',
+  },
+  caja3: {
+    padding: 20,
+    backgroundColor: 'green',
+  },
+  caja4: {
+    padding: 20,
+    backgroundColor: 'teal',
+  },
+});
+
+export default App;
